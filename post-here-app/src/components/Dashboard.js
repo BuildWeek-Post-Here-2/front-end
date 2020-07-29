a263a63208e0041ea18dd88216d341194f4f5cb2
roboblox <supeecyborg@gmail.com>
2020-07-29 16:11:17 -0400

    fixed cards placed delete button under add post

--- post-here-app/src/components/Dashboard.js
+++ post-here-app/src/components/Dashboard.js
@@ -2,40 +2,11 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Posts from "../components/login/Posts";
-import styled from "styled-components";
 import NewPost from "./NewPost";
 
 export default function Dashboard() {
-  const StyledForm = styled.div`
-    background-color: #3498db;
-    nav {
-      display: flex;
-      justify-content: space-around;
-      background-color: #d7bde2;
-      width: 100%;
-      a {
-        text-decoration: none;
-        font-size: 2rem;
-        color: #2e4053;
-      }
-    }
-    .cardBox {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      .card {
-        margin-top: 3rem;
-        background-color: black;
-        width: 50%;
-        p {
-          font-size: 2rem;
-          color: greenyellow;
-        }
-      }
-    }
-  `;
   return (
-    <div>
+    <styledForm>
       <header>
         <nav>
           <Link to="/">Signup</Link>
@@ -45,6 +16,6 @@ export default function Dashboard() {
         <NewPost />
       </header>
       <Posts />
-    </div>
+    </styledForm>
   );
 }
--- post-here-app/src/components/EditPost.js
+++ post-here-app/src/components/EditPost.js
@@ -1,71 +1,74 @@
-import React, { useState, useContext, useEffect } from 'react';
-import axiosWithAuth from '../utils/axiosWithAuth';
-import { UserContext } from '../utils/UserContext';
+import React, { useState, useContext, useEffect } from "react";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { UserContext } from "../utils/UserContext";
 
 const EditPost = (props) => {
-    const { user_id, getData } = useContext(UserContext)
+  const { user_id, getData } = useContext(UserContext);
 
-    const [postToEdit, setPostToEdit] = useState({
-        id:"",
-        user_id:"",
-        title:"",
-        content:"",
-        subreddit:"",
-    })
+  const [postToEdit, setPostToEdit] = useState({
+    id: "",
+    user_id: "",
+    title: "",
+    content: "",
+    subreddit: "",
+  });
 
-    // Web API PUT request
-    const editPost = (e, id) => {
-        e.preventDefault()
-        axiosWithAuth()
-        .put(`/api/posts/${id}`, {
-            user_id: user_id,
-            title: postToEdit.title,
-            content: postToEdit.content,
-            subreddit: postToEdit.subreddit,
-        })
-        .then(res => {
-          console.log("Post Changed", res)
-          setPostToEdit({
-            user_id: user_id,
-            title: postToEdit.title,
-            content: postToEdit.content,
-            subreddit: postToEdit.subreddit,
-          })
-          getData()
-        })
-        .catch(err => {
-          console.log(err)
-        })
-      }
-
-    const onChangeHandler = e => {
+  // Web API PUT request
+  const editPost = (e, id) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .put(`/api/posts/${id}`, {
+        user_id: user_id,
+        title: postToEdit.title,
+        content: postToEdit.content,
+        subreddit: postToEdit.subreddit,
+      })
+      .then((res) => {
+        console.log("Post Changed", res);
         setPostToEdit({
-            ...postToEdit, [e.target.name]: e.target.value
-        })
-    }
+          user_id: user_id,
+          title: postToEdit.title,
+          content: postToEdit.content,
+          subreddit: postToEdit.subreddit,
+        });
+        getData();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    return(
-        <div className='newPost'>
-            <h3>Update a Post</h3>
-            <form>
-                <input
-                    type="text"
-                    name="title"
-                    value={postToEdit.title}
-                    onChange={onChangeHandler}
-                    placeholder="title"
-                />
-                <input
-                    type="text"
-                    name="content"
-                    value={postToEdit.content}
-                    onChange={onChangeHandler}
-                    placeholder="content"
-                />
-                <button id="editbutton" onClick={(e) => editPost(e, props.id)}>Submit Changes</button>
-            </form>
-        </div>
-    )
-}
+  const onChangeHandler = (e) => {
+    setPostToEdit({
+      ...postToEdit,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-export default EditPost
+  return (
+    <div className="newPost">
+      <h3>Update Post</h3>
+      <form>
+        <input
+          type="text"
+          name="title"
+          value={postToEdit.title}
+          onChange={onChangeHandler}
+          placeholder="title"
+        />
+        <input
+          type="text"
+          name="content"
+          value={postToEdit.content}
+          onChange={onChangeHandler}
+          placeholder="content"
+        />
+        <button id="editbutton" onClick={(e) => editPost(e, props.id)}>
+          Submit Changes
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default EditPost;
